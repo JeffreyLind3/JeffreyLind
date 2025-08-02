e11c9008182d5cd8d7f0c876cf0058a4c093f3a6
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-02 13:20:04 -0400

    Jeffrey Lind 0.0.6-13: Design Navbar - Extract Navbar to Component and Move To layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,12 @@
 "use client";
 
 import Background from "@/components/Background";
-import LiquidGlass from "@/components/LiquidGlass/LiquidGlass";
-import { useRef } from "react";
 
 export default function Home() {
-  const containerRef = useRef<HTMLDivElement>(null);
   return (
-    <div
-      ref={containerRef}
-      className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[calc(100vw_*_25_/_16_*_3_+_200vh)] p-8 pb-20 gap-16 sm:p-20"
-    >
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[calc(100vw_*_25_/_16_*_3_+_200vh)] p-8 pb-20 gap-16 sm:p-20">
       <Background />
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <LiquidGlass
-          displacementScale={100}
-          blurAmount={0}
-          saturation={100}
-          aberrationIntensity={3}
-          elasticity={0.3}
-          cornerRadius={100}
-          mouseContainer={containerRef}
-          style={{
-            position: "fixed",
-            top: "6%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        >
-          <div className="p-6 w-[400px]" />
-        </LiquidGlass>
-      </main>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import LiquidGlass from "@/components/LiquidGlass/LiquidGlass";
+import { useEffect, useRef } from "react";
+
+export default function Navbar() {
+  const containerRef = useRef<HTMLElement | null>(null);
+
+  useEffect(() => {
+    containerRef.current = document.body;
+  }, []);
+
+  return (
+    <LiquidGlass
+      displacementScale={100}
+      blurAmount={0}
+      saturation={100}
+      aberrationIntensity={3}
+      elasticity={0.3}
+      cornerRadius={100}
+      mouseContainer={containerRef}
+      style={{
+        position: "fixed",
+        top: "6%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+      }}
+    >
+      <div className="p-6 w-[400px]"></div>
+    </LiquidGlass>
+  );
+}
