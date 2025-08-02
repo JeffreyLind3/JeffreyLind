1248e8862e22a52d674e3b94995c0be59ab1a0c4
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-02 16:40:29 -0400

    Jeffrey Lind 0.0.6-19: Design Navbar - Use @ Import Alias

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import "@/app/globals.css";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
