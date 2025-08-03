4cefacc4d40d5043591d779e60119d703e8cc180
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-03 16:13:07 -0400

    Jeffrey Lind 0.0.6-37: Design Navbar - Fix Career, Education, and Project Page Function Names

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,7 +2,7 @@
 
 import Background from "@/components/Background";
 
-export default function Home() {
+export default function Projects() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[calc(100vw_*_25_/_16_*_3_+_200vh)] p-8 pb-20 gap-16 sm:p-20">
       <Background />
