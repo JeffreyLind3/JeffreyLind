3eb020b712c4b83b078b7a54697c512f1307bd92
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-03 10:19:35 -0400

    Jeffrey Lind 0.0.6-26: Design Navbar - Add Navbar Items Text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import LiquidGlass from "@/components/LiquidGlass/LiquidGlass";
+import Link from "next/link";
 import { useEffect, useRef } from "react";
 
 export default function Navbar() {
@@ -14,14 +15,19 @@ export default function Navbar() {
     <LiquidGlass
       displacementScale={100}
       mouseContainer={containerRef}
+      padding="16px 30px"
       style={{
         position: "fixed",
-        top: "5%",
+        top: "4.7%",
         left: "50%",
         transform: "translate(-50%, -50%)",
       }}
     >
-      <div className="p-1 w-[400px]" />
+      <div className="flex justify-between w-[300px] text-base font-bold">
+        <Link href="#">Home</Link>
+        <Link href="#">Career</Link>
+        <Link href="#">Projects</Link>
+      </div>
     </LiquidGlass>
   );
 }
