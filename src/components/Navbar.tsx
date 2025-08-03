e649d10569e7a48b3ad1828791c47e118a67df07
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-03 10:54:49 -0400

    Jeffrey Lind 0.0.6-28: Design Navbar - Add Wordmark Logo to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,22 +12,38 @@ export default function Navbar() {
   }, []);
 
   return (
-    <LiquidGlass
-      displacementScale={100}
-      mouseContainer={containerRef}
-      padding="16px 30px"
-      style={{
-        position: "fixed",
-        top: "4.7%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <div className="flex justify-between w-[300px] text-base font-bold">
-        <Link href="#">Home</Link>
-        <Link href="#">Career</Link>
-        <Link href="#">Projects</Link>
-      </div>
-    </LiquidGlass>
+    <>
+      <span
+        style={{
+          position: "fixed",
+          top: "4.7%",
+          left: "calc(50% - 285px)",
+          transform: "translate(-50%, -50%)",
+          fontSize: "1.5rem",
+          fontWeight: "bold",
+          fontFamily: "var(--font-geist-sans)",
+          color: "white",
+        }}
+      >
+        Jeffrey Lind
+      </span>
+      <LiquidGlass
+        displacementScale={100}
+        mouseContainer={containerRef}
+        padding="16px 30px"
+        style={{
+          position: "fixed",
+          top: "4.7%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <div className="flex justify-between w-[300px] text-base font-bold">
+          <Link href="#">Home</Link>
+          <Link href="#">Career</Link>
+          <Link href="#">Projects</Link>
+        </div>
+      </LiquidGlass>
+    </>
   );
 }
