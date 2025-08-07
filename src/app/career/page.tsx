1af828ae1c1a8128b017577cf0e65d383225c5c2
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-06 21:44:04 -0400

    Jeffrey Lind 0.0.6-70: Design Navbar - Add Headers to Career, Education, and Projects Pages

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -4,9 +4,11 @@ import Background from "@/components/Background";
 
 export default function Career() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[100vh] p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-[100vh] p-8 pb-20 gap-16 sm:p-20">
       <Background />
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1 className="text-5xl font-bold">Career</h1>
+      </main>
     </div>
   );
 }
