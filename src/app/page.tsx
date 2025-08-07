0b984f7058dcc20fe793d0f80c53ff191a6093b3
JeffreyLind3 <jeffrey@jeffreylind.dev>
2025-08-06 21:58:27 -0400

    Jeffrey Lind 0.0.6-73: Design Navbar - Add Home Page Headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,13 @@ import Background from "@/components/Background";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[100vh] p-8 pb-20 gap-16 sm:p-20">
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-[100vh] p-8 pb-20 gap-16 sm:p-20">
       <Background />
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
+      <main className="flex flex-col gap-[32px] row-start-2 items-center">
+        <h1 className="text-5xl font-bold">Beauty</h1>
+        <div className="text-5xl font-bold">Elegance</div>
+        <div className="text-5xl font-bold">Innovation</div>
+      </main>
     </div>
   );
 }
